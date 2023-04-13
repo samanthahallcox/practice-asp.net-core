0625d2d44e681b9483a79d57480d49c3842434e7
Samantha Hallcox <samanthahallcox@users.noreply.github.com>
2023-04-13 14:17:14 -0400

    First commit

---
+++ Projects/DemoMVCFromVS/SampleDotNetMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleDotNetMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
